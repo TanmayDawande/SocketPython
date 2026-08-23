ff1d077feeb4c0bc3bbe429e6c3b3cfe529fa15c
Tanmay <tdawande007@gmail.com>
2026-08-23 10:59:53 +0530

    added a 4 byte header to all packets. this header  tells the reciever exacltly how many byte message the packet has dynamically ensuring efficient transfer.

--- client.py
+++ client.py
@@ -1,18 +1,30 @@
 import socket
+import struct
 
 host = '127.0.0.1' 
 port = 65432
+
+def pack(var):
+    packed = struct.pack("!I", len(var.encode('utf-8')))
+    return packed+var.encode('utf-8')
+    # Creating a packet where first bits are the length of the message
+    # ! is for standard network byte order and I is for unsigned int
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((host, port))
     print(f"(+) Connected to {host}:{port}")
 
     while True:
-        message = input("Message: ")
-        client_socket.sendall(message.encode('utf-8'))
+        message = input("message: ")
+        client_socket.sendall(pack(message))
         print("(+) Message sent to server...")
 
-        data = client_socket.recv(1024)
-        print(f"Message from server: {data.decode('utf-8')}")
+
+        data_bits_struct = client_socket.recv(4)
+        data_bits = struct.unpack("!I", data_bits_struct)
+        data = client_socket.recv(data_bits[0])
+        print(f"reply: {data.decode('utf-8')}")
+        print("(+) Waiting for the message...")
 
 print("(+)Connection closed")
--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 import socket
+import struct
 
 host  = '127.0.0.1'
 port = 65432
+
+def pack(var):
+    packed = struct.pack("!I", len(var.encode('utf-8')))
+    return packed+var.encode('utf-8')
+    # Creating a packet where first bits are the length of the message
+    # ! is for standard network byte order and I is for unsigned int
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind((host, port))
@@ -11,15 +18,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     with conn:
         print(f"(+) Connected by {addr}")
         while True:
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                if data.decode('utf-8') == "Close":
-                    break
 
-                print(f"The message is: {data.decode('utf-8')}")
-                message = input("reply: ")
-                conn.sendall(message.encode("utf-8"))
+            data_bits_struct = conn.recv(4)
+            data_bits = struct.unpack("!I", data_bits_struct)
+            data = conn.recv(data_bits[0])
+            if not data:
+                break
+            if data.decode('utf-8') == "Close":
+                break
+
+            print(f"message: {data.decode('utf-8')}")
+            message = input("reply: ")
+            print("(+) Waiting for the message...")
+            conn.sendall(pack(message))
 
 print("(+) Connection Closed")
